d0c776f8df37858213b98091f889ee531aebaa4a
SalesMga <lucassalessa3@gmail.com>
2020-05-18 12:24:26 -0300

    Estrutura e Rotas configuradas

---
+++ src/pages/Signin/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Signin() {
+  return <div />;
+}
+
+export default Signin;
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+
+import Signin from '../pages/Signin';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pagesProfile';
+
+export default function Routes(){
+    return (
+        <Switch>
+            <Route path="/" exact component={Signin}/>
+            <Route path="/register"  component={SignUp}/>
+
+            <Route path="/dashboard"  component={Dashboard}/>
+            <Route path="/profile"  component={Profile}/>
+        </Switch>
+    );
+}
