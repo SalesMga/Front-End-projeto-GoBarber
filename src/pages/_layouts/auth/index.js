3685233dc96e5d08e59729f7852a9c9ee3bbdc75
SalesMga <lucassalessa3@gmail.com>
2020-05-20 11:50:08 -0300

    Layout por paginas

---
+++ src/pages/_layouts/auth/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Wrapper } from './styles';
+import PropTypes from 'prop-types';
+
+export default function AuthLayout({ children }) {
+    return (
+        <Wrapper>{children}</Wrapper>
+    );
+}
+
+AuthLayout.propTypes = {
+children: PropTypes.element.isRequired,
+}
