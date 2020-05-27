ef0e032cdad740adb3c08274378b0fd68449e1b8
SalesMga <lucassalessa3@gmail.com>
2020-05-27 13:08:37 -0300

    Configurando o Store (redux, redux saga e reactotron-saga)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import Routes from './routes';
 import history from './services/history';
 
 import './Config/ReactotronConfig';
+
+import store from './store';
+
 import GLobalStyle from './styles/global';
 
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GLobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GLobalStyle />
+            </Router>
+        </Provider>
     );
 }
 
