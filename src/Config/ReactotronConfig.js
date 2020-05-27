4a05d9105ec11b0f04ff45ba594f792d2633356e
SalesMga <lucassalessa3@gmail.com>
2020-05-27 16:53:45 -0300

    Autenticação (buscando dados da api e direcionando user a page da dash)

--- src/Config/ReactotronConfig.js
+++ src/Config/ReactotronConfig.js
@@ -3,7 +3,8 @@ import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
 if( process.env.NODE_ENV === 'development') {
-    const tron = Reactotron.configure().connect()
+    const tron = Reactotron
+    .configure()
     .use(reactotronRedux())
     .use(reactotronSaga())
     .connect();
--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,17 @@ import createStore from './modules/createStore';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
+import Reactotron from '../Config/ReactotronConfig';
 
-const sagaMonitor =
-process.env.NODE_ENV == 'development'
+const sagaMonitor = process.env.NODE_ENV === 'development'
 ? console.tron.createSagaMonitor()
 : null;
 
 const sagaMiddleware = createSagaMiddleware({sagaMonitor});
 
-const middlawares = [sagaMiddleware];
+const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlawares);
+const store = createStore(rootReducer, middlewares);
 
 sagaMiddleware.run(rootSaga);
 
