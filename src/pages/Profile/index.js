c9384b2e75f7dd57c6322fb7d86b016323003257
SalesMga <lucassalessa3@gmail.com>
2020-07-15 21:52:50 -0300

    Pagina de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-function Profile() {
-  return <div />;
+export default function Profile() {
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit (data) {
+
+    }
+    return(
+
+    <Container>
+        <Form initialData={profile} onSubmit={handleSubmit} >
+            <Input name="name" placeholder="Nome completo" />
+                <Input name="email" type="email" placeholder="Se e-mail" />
+
+                    <hr />
+
+                    <Input type="password" name="oldPassword" placeholder="Sua senha atual" />
+                        <Input type="password" name="Password" placeholder="Nova senha" />
+                            <Input type="password" name="ConfirmPassword" placeholder="Confirma senha" />
+                                <button type="submit">Atualizar perfil</button>
+      </Form>
+
+                            <button type="submit">Sair do GoBarber</button>
+
+  </Container>
+);
 }
-
-export default Profile;
