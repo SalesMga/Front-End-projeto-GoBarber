7ab0bc16e983eea4bc853401815e9e261950f0b8
SalesMga <lucassalessa3@gmail.com>
2020-08-25 21:36:11 -0300

    Navegando entre os dias

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,39 +1,56 @@
 import React from 'react';
 import api from '../../services/api';
-import {MdChevronRight,MdChevronLeft} from 'react-icons/md';
+import { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
-    return (
-    <Container>
-        <header>
-            <button type="button">
-                <MdChevronLeft size={36} color="#FFF" />
-            </button>
-            <strong>31 de maio</strong>
-            <button type="button">
-                <MdChevronRight size={36} color="#FFF" />
-            </button>
-        </header>
+    const [date, setDate] = useState(new Date());
 
-        <ul>
-            <Time past>
-                <strong>08:00</strong>
-                <span>Diego Fernandes</span>
-            </Time>
-            <Time available>
-                <strong>09:00</strong>
-                <span>Em aberto</span>
-            </Time>
-            <Time>
-                <strong>10:00</strong>
-                <span>Diego Fernandes</span>
-            </Time>
-            <Time>
-                <strong>11:00</strong>
-                <span>Diego Fernandes</span>
-            </Time>
-        </ul>
-    </Container>
+    const dateFormatted = useMemo(
+        () => format(date, "d 'de' MMMM", { locale: pt }),
+        [date]
+    );
+
+    function handlePrevDay () {
+        setDate(subDays(date, 1));
+    }
+    function handleNextDay () {
+        setDate(addDays(date, 1));
+    }
+
+    return (
+        <Container>
+            <header>
+                <button type="button" onClick={handlePrevDay}>
+                    <MdChevronLeft size={36} color="#FFF" />
+                </button>
+                <strong>{dateFormatted}</strong>
+                <button type="button" onClick={handleNextDay}>
+                    <MdChevronRight size={36} color="#FFF" />
+                </button>
+            </header>
+
+            <ul>
+                <Time past>
+                    <strong>08:00</strong>
+                    <span>Diego Fernandes</span>
+                </Time>
+                <Time available>
+                    <strong>09:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+                <Time>
+                    <strong>10:00</strong>
+                    <span>Diego Fernandes</span>
+                </Time>
+                <Time>
+                    <strong>11:00</strong>
+                    <span>Diego Fernandes</span>
+                </Time>
+            </ul>
+        </Container>
     );
 }
