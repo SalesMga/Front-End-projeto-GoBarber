864b6c85f6185773ff5fe8c22a62f16cccbf7db4
SalesMga <lucassalessa3@gmail.com>
2020-08-25 21:16:00 -0300

    Estilização do Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import api from '../../services/api';
+import {MdChevronRight,MdChevronLeft} from 'react-icons/md';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-    api.get('appointments');
+    return (
+    <Container>
+        <header>
+            <button type="button">
+                <MdChevronLeft size={36} color="#FFF" />
+            </button>
+            <strong>31 de maio</strong>
+            <button type="button">
+                <MdChevronRight size={36} color="#FFF" />
+            </button>
+        </header>
 
-    return <div>Dashboard</div>;
+        <ul>
+            <Time past>
+                <strong>08:00</strong>
+                <span>Diego Fernandes</span>
+            </Time>
+            <Time available>
+                <strong>09:00</strong>
+                <span>Em aberto</span>
+            </Time>
+            <Time>
+                <strong>10:00</strong>
+                <span>Diego Fernandes</span>
+            </Time>
+            <Time>
+                <strong>11:00</strong>
+                <span>Diego Fernandes</span>
+            </Time>
+        </ul>
+    </Container>
+    );
 }
