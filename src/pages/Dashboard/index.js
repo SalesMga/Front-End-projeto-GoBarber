fd7981111b12e577bebcbef069d24032762a2a24
SalesMga <lucassalessa3@gmail.com>
2020-08-26 22:56:59 -0300

    Listando agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import api from '../../services/api';
-import { useState, useMemo } from 'react';
-import { format, subDays, addDays } from 'date-fns';
+import { useState, useMemo, useEffect } from 'react';
+import { format, subDays, addDays, setHours, setMinutes, setSeconds, isBefore, isEqual, parseISO } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
 import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+    const [schedule, setSchedule] = useState([]);
     const [date, setDate] = useState(new Date());
 
     const dateFormatted = useMemo(
@@ -14,10 +18,37 @@ export default function Dashboard() {
         [date]
     );
 
-    function handlePrevDay () {
+    useEffect(() => {
+        async function loadScheadule() {
+            const response = await api.get('schedule', {
+                params: { date },
+            });
+
+            const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+            const data = range.map(hour => {
+                const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+                const compareDate = utcToZonedTime(checkDate, timezone);
+
+                return {
+                    time: `${hour}:00h`,
+                    past: isBefore(compareDate, new Date()),
+                    appointment: response.data.find(a =>
+                        isEqual(parseISO(a.date), compareDate),
+                    ),
+                };
+            });
+
+            setSchedule(data);
+        }
+
+        loadScheadule();
+    }, [date]);
+
+    function handlePrevDay() {
         setDate(subDays(date, 1));
     }
-    function handleNextDay () {
+    function handleNextDay() {
         setDate(addDays(date, 1));
     }
 
@@ -34,22 +65,12 @@ export default function Dashboard() {
             </header>
 
             <ul>
-                <Time past>
-                    <strong>08:00</strong>
-                    <span>Diego Fernandes</span>
-                </Time>
-                <Time available>
-                    <strong>09:00</strong>
-                    <span>Em aberto</span>
-                </Time>
-                <Time>
-                    <strong>10:00</strong>
-                    <span>Diego Fernandes</span>
-                </Time>
-                <Time>
-                    <strong>11:00</strong>
-                    <span>Diego Fernandes</span>
-                </Time>
+                {schedule.map(time => (
+                    <Time key={time.time} past={time.past} available={!time.appointment}>
+                        <strong>{time.time}</strong>
+                        <span>{time.appointment ? time.appointment.user.name : 'Em aberto'}</span>
+                    </Time>
+                ))}
             </ul>
         </Container>
     );
